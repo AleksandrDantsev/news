5f48c783985745a0fbd7a39ad18b85679736a198
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-05 11:53:54 +0300

    feat: create page

---
+++ src/#template/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import st from "./Main.module.scss";
+
+const Main:React.FC = () => {
+    return(
+        <div className={st.news_column}>
+
+        </div>
+    );
+}
+
+export default Main;
