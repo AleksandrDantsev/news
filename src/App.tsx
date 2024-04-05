3deb79d2b016b6084ed2b7e59cb1203ad9326c04
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-05 11:52:35 +0300

    chore: add Main pagу in router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Fragment } from 'react/jsx-dev-runtime';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss'
 import Layout from './layout/Layout';
+import NewsQuickCard from './components/NewsQuickCard/NewsQuickCard';
+import Main from './pages/Main/Main';
 
 function App() {
 
@@ -9,7 +11,7 @@ function App() {
     <Fragment>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/r" element={<div>n,nm,yuy</div>}/>
+          <Route index element={<Main />}/>
           <Route path="/2" element={<div>n,nm,yeeeeeeuy</div>}/>
         </Route>
       </Routes>
