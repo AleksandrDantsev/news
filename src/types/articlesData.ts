de372bb8cf986b61aea94660f121083f93a89a53
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-07 12:12:52 +0300

    chore: add types of data

---
+++ src/types/articlesData.ts
@@ -0,0 +1,9 @@
+export type TArticlesData = {
+    sourse: {};
+    title: string;
+    description: string;
+    content: string;
+    author?: string;
+    urlToImage: string;
+    publishedAt?: string;
+}
