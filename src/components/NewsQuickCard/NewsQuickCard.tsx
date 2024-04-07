2874bfd5f5f9e21b7eb11ae5fd669701e2daa707
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-07 12:03:44 +0300

    feat: add data

--- src/components/NewsQuickCard/NewsQuickCard.tsx
+++ src/components/NewsQuickCard/NewsQuickCard.tsx
@@ -2,11 +2,18 @@ import React, { memo } from "react";
 import ButtonReadLater from "../../UI/ButtonReadLater/ButtonReadLater";
 import st from "./NewsQuickCard.module.scss";
 
-const NewsQuickCard:React.FC = memo(() => {
+
+interface INewsQuickCard {
+    title: string;
+    author?: string;
+    img: string;
+}
+
+const NewsQuickCard:React.FC<INewsQuickCard> = memo(({title, author, img}) => {
     return(
         <div className={st.news_quick_card}>
             <div className={st.news_quick_card__img}>
-                <img src="https://www.washingtonpost.com/wp-apps/imrs.php?sr…%2FBTRD6VIFQ5HQJLNVFRCWSK4JSQ%252Ejpg&w=464&h=310" alt="" />
+                <img src={img} alt={title} />
                 <div className={st.button_absolute_block}>
                     <ButtonReadLater />
                 </div>
@@ -14,12 +21,10 @@ const NewsQuickCard:React.FC = memo(() => {
             <div className={st.news_quick_card__bottom_info}>
                 <div className={st.news_quick_card__name}>
                     <h3 className={st.news_quick_card__name_title}>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio 
-                        perspiciatis dolorem eveniet deserunt
-                        
+                        {title}
                     </h3>
                 </div>
-                <div className={st.news_quick_card__author}>author</div>
+                <div className={st.news_quick_card__author}>{author}</div>
             </div>
         </div>
     );
