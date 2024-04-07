f5b06b742b1c931dbb3bb8fa88721fedce4c144e
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-07 12:02:41 +0300

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import st from "./Header.module.scss";
+
+const Header:React.FC = () => {
+    return(
+        <header className={st.header}>
+
+        </header>
+    );
+}
+
+export default Header;
