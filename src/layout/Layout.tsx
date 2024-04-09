8b37c717e3b3780b4ac3d77a3c20bc86685e2b9c
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-09 14:10:52 +0300

    chore: add general wrapper

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,17 @@
 import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header/Header";
+import st from "./Laoyut.module.scss";
 
 const Layout:React.FC = () => {
     return(
         <Fragment>
             <Header />
-            <main>
-                <Outlet />
-            </main>
+            <div className={st.wrapper}>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
         </Fragment>
     );
 }
