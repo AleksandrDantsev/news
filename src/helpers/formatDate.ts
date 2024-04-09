17c5a30508b0602ed66abfc9b3407b7a2d999113
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-09 14:09:14 +0300

    chore: add function format date

---
+++ src/helpers/formatDate.ts
@@ -0,0 +1,19 @@
+// get format date "2024-05-29"
+
+const formatDate = (shiftBackDay = 0) => {
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const mounth = date.getMonth();
+    const day = date.getDate();
+
+    const rightDate = new Date(year, mounth, day - shiftBackDay);
+
+    const dayCount = String(rightDate.getDate()).padStart(2, '0')
+    const mounthCount = String(rightDate.getMonth() + 1).padStart(2, '0');
+    const yearCount = String(rightDate.getFullYear());
+
+    return `${yearCount}-${mounthCount}-${dayCount}`
+}
+
+export { formatDate };
