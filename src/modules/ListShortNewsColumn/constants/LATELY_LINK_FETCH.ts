e818ba6ff582e773910160cfbe6790b84da001d2
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-09 14:13:46 +0300

    chore: create link

--- src/modules/ListShortNewsColumn/constants/LATELY_LINK_FETCH.ts
+++ src/modules/ListShortNewsColumn/constants/LATELY_LINK_FETCH.ts
@@ -1,1 +1,4 @@
-export const LATELY_LINK_FETCH  = `https://newsapi.org/v2/top-headlines?country=us`;
+import { formatDate } from "../../../helpers/formatDate";
+
+const fromDate = formatDate(1);
+export const LATELY_LINK_FETCH  = `https://newsapi.org/v2/everything?q=new&from=${fromDate}`;
