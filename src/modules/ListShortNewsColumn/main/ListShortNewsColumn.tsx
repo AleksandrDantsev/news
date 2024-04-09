9b036bd9810a21e4a80b4a0a1f396d6cfa20ef9a
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-09 14:17:31 +0300

    chore: add info props

--- src/modules/ListShortNewsColumn/main/ListShortNewsColumn.tsx
+++ src/modules/ListShortNewsColumn/main/ListShortNewsColumn.tsx
@@ -2,28 +2,26 @@ import React, { memo } from "react";
 import st from "./ListShortNewsColumn.module.scss";
 import ListShortNewsColumnItem from "../components/ListShortNewsColumnItem/ListShortNewsColumnItem";
 import { TArticlesData } from "../../../types/articlesData";
-import { getReadTime } from "../helpers/getReadTime";
 import { useFetchData } from "../../../hook/useFetchData";
 import { LATELY_LINK_FETCH } from "../constants/LATELY_LINK_FETCH";
 import { filterNonexistentNews } from "../../../helpers/filterField";
 
-
 const ListShortNewsColumn:React.FC = memo(() => {
     const [articles] = useFetchData<TArticlesData[]>(LATELY_LINK_FETCH);
+    const filteredData = filterNonexistentNews<TArticlesData>(articles, ["title", "content"], ["[Removed]", null], 4)
 
 
-    const filteredData = filterNonexistentNews<TArticlesData>(articles, "title", "[Removed]", 4)
-    console.log(filteredData)
     return(
         <div className={st.short_news_column}>
             {
-                articles.length > 0 ? 
-                filteredData.map(article => <ListShortNewsColumnItem 
-                                                                            title={article?.["title"]}
-                                                                            author={article?.["author"]}
-                                                                            img={article?.["urlToImage"]}
-                                                                            timeRead={getReadTime(article['content']?.length ?? 0)}
-                                                                                />  ) : ''
+                filteredData.length > 0 ? 
+                filteredData.map((article, i) => <ListShortNewsColumnItem key={article?.["title"] + i}
+                                                                    title={article?.["title"]}
+                                                                    author={article?.["author"]}
+                                                                    img={article?.["urlToImage"]}
+                                                                    publishedTime={article?.["publishedAt"]}
+                                                                    timeReadNumber={article['content']?.length || 0}
+                                                                    />  ) : ''
             }
         </div>
     );
