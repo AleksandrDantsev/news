c3911e262c7165df0bb2e674560ce965a8a6bed6
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-09 14:17:08 +0300

    chore: add number transformation

--- src/modules/ListShortNewsColumn/tests/getReadTime.test.ts
+++ src/modules/ListShortNewsColumn/tests/getReadTime.test.ts
@@ -3,13 +3,13 @@ import { getReadTime } from "../helpers/getReadTime";
 
 describe("check for get time read", () => {
     it('check 1500 letters', () => {
-        expect(Number(getReadTime(1500))).toBeGreaterThanOrEqual(3);
+        expect(Number(getReadTime(1500))).toBeGreaterThanOrEqual(2);
     })
     it('check 950 letters', () => {
         expect(Number(getReadTime(950))).toBeGreaterThanOrEqual(2);
     })
     it('check 12950 letters', () => {
-        expect(Number(getReadTime(12950))).toBeGreaterThanOrEqual(28);
+        expect(Number(getReadTime(12950))).toBeGreaterThanOrEqual(22);
     })
     it('check 0 letters', () => {
         expect(getReadTime(0)).toBe("few");
