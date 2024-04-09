78018de3e76f2c86f9fa1df71dc98f2d63bca800
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-09 14:09:41 +0300

    chore: add function readable view date

---
+++ src/helpers/formatDateArticleCreated.ts
@@ -0,0 +1,14 @@
+const formatDateArticleCreated = (textDate: string | undefined) => {
+    if (!Boolean(textDate)) return '';
+    
+    const options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        timezone: 'UTC'
+      } as const;
+
+    return new Date(textDate!.split("T")[0]).toLocaleString("ru", options);
+}
+
+export { formatDateArticleCreated };
