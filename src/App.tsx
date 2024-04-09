1bbed2a364d0957c05a9fbddbc6ddbe55f8a23e2
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-09 14:05:44 +0300

    feat: add new page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import { Fragment } from 'react/jsx-dev-runtime';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss'
 import Layout from './layout/Layout';
-import NewsQuickCard from './components/NewsQuickCard/NewsQuickCard';
 import Main from './pages/Main/Main';
+import Article from './pages/Article/Article';
 
 function App() {
-
   return (
     <Fragment>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Main />}/>
-          <Route path="/2" element={<div>n,nm,yeeeeeeuy</div>}/>
+          <Route path="/1" element={<Article />}/>
         </Route>
       </Routes>
     </Fragment>
