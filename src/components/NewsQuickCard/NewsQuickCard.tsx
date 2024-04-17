d774539d9783023e7db76825f8cc8ffe67d76597
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-17 11:53:59 +0300

    feat: add behavior for button

--- src/components/NewsQuickCard/NewsQuickCard.tsx
+++ src/components/NewsQuickCard/NewsQuickCard.tsx
@@ -2,29 +2,33 @@ import React, { memo } from "react";
 import ButtonReadLater from "../../UI/ButtonReadLater/ButtonReadLater";
 import st from "./NewsQuickCard.module.scss";
 import { ImageLazy } from "../../hoc/ImageLazy";
+import { TArticlesData } from "../../types/articlesData";
+import { useActionForCollections } from "../../hook/useActionForCollections";
 
 interface INewsQuickCard {
-    title: string;
-    author?: string;
-    img: string;
+    data: TArticlesData;
 }
 
-const NewsQuickCard:React.FC<INewsQuickCard> = memo(({title, author, img}) => {
+const NewsQuickCard:React.FC<INewsQuickCard> = memo(({data}) => {
+    const [isAddedToCollections, setIsAddedToCollections] = useActionForCollections(data["title"], data);
+
+
     return(
         <div className={st.news_quick_card}>
             <div className={st.news_quick_card__img}>
-                <ImageLazy effect={"blur"} src={img} alt={title} height={"100%"} />
+                <ImageLazy effect={"blur"} src={data["urlToImage"]} alt={data["title"]} height={"100%"} />
                 <div className={st.button_absolute_block}>
-                    <ButtonReadLater />
+                    <ButtonReadLater addToCollections={setIsAddedToCollections}
+                                     isAddedToCollections={isAddedToCollections}/>
                 </div>
             </div>
             <div className={st.news_quick_card__bottom_info}>
                 <div className={st.news_quick_card__name}>
                     <h3 className={st.news_quick_card__name_title}>
-                        {title}
+                        {data["title"]}
                     </h3>
                 </div>
-                <div className={st.news_quick_card__author}>{author}</div>
+                <div className={st.news_quick_card__author}>{data["author"]}</div>
             </div>
         </div>
     );
