833b417a0c0635fbc0382aa50302558941e71729
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-17 12:16:37 +0300

    fix: change image-lazy component and add interface

--- src/modules/ArticleUnit/components/HeadArticle/HeadArticle.tsx
+++ src/modules/ArticleUnit/components/HeadArticle/HeadArticle.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import st from "./HeadArticle.module.scss";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { ImageLazy } from "../../../../hoc/ImageLazy";
+import { formatDateArticleCreated } from "../../../../helpers/formatDateArticleCreated";
 
-const HeadArticle:React.FC = () => {
+interface IHeadArticle {
+    title: string;
+    image: string;
+    description: string;
+    date?: string;
+    author?: string;
+}
+
+
+const HeadArticle:React.FC<IHeadArticle> = ({ title, image, description, date, author }) => {
+    const formatDate = formatDateArticleCreated(date);
+
     return(
         <div className={st.article_head}>
+            <div className={st.article_head__date}>{formatDate}</div>
             <div className={st.article_title_block}>
-                <h1 className={st.article_title}>werw</h1>
-                <div className={st.article_subhead}>wescxvcxv</div>
+                <h1 className={st.article_title}>{title}</h1>
+                <div className={st.article_subhead}>{description}</div>
             </div>
-            <div className={st.article_head__author}>name</div>
+            <div className={st.article_head__author}>{author}</div>
             <div className={st.article_head__poster}>
-                <LazyLoadImage effect="blur" src={"."} alt=""/> 
+                <ImageLazy effect="blur" src={image} alt={title} height="100%" width="100%"/> 
             </div>
         </div>
     );
