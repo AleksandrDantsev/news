7fb35ddf46e9cb61d9e5a55d6733121504ca2bcf
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-17 12:37:16 +0300

    style: change the background color

--- src/UI/ButtonReadLater/ButtonReadLater.tsx
+++ src/UI/ButtonReadLater/ButtonReadLater.tsx
@@ -1,12 +1,22 @@
 import React, { memo } from "react";
 import ReadLaterIcon from "../../assets/svg/readLaterIcon.svg";
+import AddedToReadLaterSvg from "../../assets/svg/tick.svg";
 import st from "./ButtonReadLater.module.scss";
 
-const ButtonReadLater:React.FC = memo(() => {
+interface IButtonReadLater {
+    addToCollections: () => void;
+    isAddedToCollections: boolean;
+}
+
+const ButtonReadLater:React.FC<IButtonReadLater> = memo(({addToCollections, isAddedToCollections}) => {
     return(
-        <div className={st.bt_read_later}>
-            <img src={ReadLaterIcon} alt="read later" />
-        </div>
+        <button className={st.bt_read_later} 
+                onClick={addToCollections}>
+            {
+                isAddedToCollections ? <img src={AddedToReadLaterSvg} alt="added to collections" /> : 
+                                       <img src={ReadLaterIcon} alt="read later" />
+            }
+        </button>
     );
 })
 
