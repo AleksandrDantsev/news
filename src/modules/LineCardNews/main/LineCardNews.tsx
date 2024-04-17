4125dfbe1bff4cf180d495045d5d614a764b4abd
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-17 12:22:24 +0300

    refactor: remove the spaces

--- src/modules/LineCardNews/main/LineCardNews.tsx
+++ src/modules/LineCardNews/main/LineCardNews.tsx
@@ -2,27 +2,37 @@ import React from "react";
 import st from "./LineCardNews.module.scss";
 import NewsQuickCard from "../../../components/NewsQuickCard/NewsQuickCard";
 import { TArticlesData } from "../../../types/articlesData";
+import { useFetchData } from "../../../hook/useFetchData";
+import { filterNonexistentNews } from "../../../helpers/filterField";
+import { SPORT_LINK, TECHNOLOGY_LINK } from "../constants/FETCH_LINKS";
 
-interface ILineCardNes {
-    dataArray: TArticlesData[];
+interface ILineCardNews {
+    category: string;
+    titleNews: string;
 }
 
-const LineCardNews:React.FC<ILineCardNes> = ({dataArray}) => {
-    
+const links = {
+    sport: SPORT_LINK,
+    technology:  TECHNOLOGY_LINK,
+}
 
+const LineCardNews:React.FC<ILineCardNews> = ({category, titleNews}) => {
+    const [data] = useFetchData<TArticlesData[]>(links[category as keyof typeof links]);
+    const filteredData = filterNonexistentNews(data, ['urlToImage', "title"], [null, "[Removed]"], 4)
+    console.log(filteredData)
     return(
         <section className={st.line_news}>
-            <div className={st.title}><h2>news</h2></div>
+            <div className={st.title}>
+                <h2>{titleNews}</h2>
+            </div>
             <div className={st.news_cards}>
                 {
-                    dataArray.length > 0 ? dataArray.map((article, i) => <NewsQuickCard key={article["title"] + i}
-                                                                                 title={article["title"]}
-                                                                                 author={article?.["author"]}
-                                                                                 img={article?.["urlToImage"]}
-                                                                                />) : ""
-
+                    filteredData.length > 0 ? filteredData.map((article, i) => (
+                    <div className={st.cart_cont} key={article["title"] + i}>
+                        <NewsQuickCard data={article} />
+                    </div>
+                    )) : ""
                 }
-
             </div>
         </section>
     );
