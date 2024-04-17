8e554c2181fbc187b8b5cf4e980fde75ca708063
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-17 12:34:36 +0300

    feat: add the unlimited news

--- src/modules/TilesNews/main/TilesNews.tsx
+++ src/modules/TilesNews/main/TilesNews.tsx
@@ -1,27 +1,44 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import st from "./TilesNews.module.scss";
 import TileNewItem from "../components/TileNewItem/TileNewItem";
 import { useFetchData } from "../../../hook/useFetchData";
 import { TArticlesData } from "../../../types/articlesData";
 import { filterNonexistentNews } from "../../../helpers/filterField";
 import { TILE_LINK_DATA } from "../constants/TILE_LINK_DATA";
+import { ButtonRegular } from "../../../UI/ButtonRegular/ButtonRegular";
 
-const TilesNews:React.FC = memo(() => {
+interface ITilesNews {
+    titleNews: string;
+}
+
+const TilesNews:React.FC<ITilesNews> = memo(({titleNews}) => {
     const [articles] = useFetchData<TArticlesData[]>(TILE_LINK_DATA);
-    
-    const filteredNewsSlice = filterNonexistentNews<TArticlesData>(articles, ["urlToImage", "title"], [null, "[Removed]"])
+    const [quantityNews, setQuantityNews] = useState<number>(0);
+    const filteredNewsSlice = filterNonexistentNews<TArticlesData>(articles, ["urlToImage", "title"], [null, "[Removed]"], 100)
 
-    console.log(filteredNewsSlice)
+    const slicedArrayNews = (() => filteredNewsSlice.slice(0, quantityNews + 5))()
+
+    const plusSliceArray =() => {
+        quantityNews < filteredNewsSlice.length && setQuantityNews(quantityNews + 10);
+    }
+
+    
     return(
         <section className={st.tiles_news}>
+            <h2 className={st.title_popular_news}>{titleNews}</h2>
             {
-            articles.length > 0 ? filteredNewsSlice.map((article, i) => <TileNewItem key={article?.["title"] + i}
-                                                                        title={article?.["title"]}
-                                                                        description={article?.["description"]}
-                                                                        img={article?.["urlToImage"]}
-                                                                    />) : ''
+            articles.length > 0 ? slicedArrayNews.map((article, i) => <TileNewItem key={article?.["title"] + i}
+                                                                                        data={article}
+                                                                                        />) : ''
             }
-            
+            {quantityNews < filteredNewsSlice.length && 
+                <div className={st.button_read_more}>
+                    <ButtonRegular  text={"Read more news"} 
+                                    classname={"read_more_bt"}
+                                    actFunc={plusSliceArray}
+                                    />
+                </div>
+            }
         </section>
     );
 })
