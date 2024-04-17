b522188a18fe1f884545160dd827cce7474b2813
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-17 12:40:51 +0300

    feat: add page for see the added articles

---
+++ src/pages/ReadLaterCollection/ReadLaterCollection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import st from "./ReadLaterCollection.module.scss";
+import { ReadLaterList } from "../../modules/ReadLaterList/main/ReadLaterList";
+
+
+const ReadLaterCollection:React.FC = () => {
+
+    return(
+        <ReadLaterList />
+    );
+}
+
+export { ReadLaterCollection };
